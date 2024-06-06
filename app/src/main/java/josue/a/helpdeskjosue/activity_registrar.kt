f015d379eecb57168d28dee1b5ca9e449a833a6d
josue <20230098@ricaldone.edu.sv>
2024-06-06 11:10:56 -0600

    Commit numero 6

--- app/src/main/java/josue/a/helpdeskjosue/activity_registrar.kt
+++ app/src/main/java/josue/a/helpdeskjosue/activity_registrar.kt
@@ -1,6 +1,8 @@
 package josue.a.helpdeskjosue
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class activity_registrar : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnRegistrar: Button = findViewById(R.id.btnInicioR)
+        btnRegistrar.setOnClickListener {
+            val intent = Intent(this, activity_Inicio::class.java)
+            startActivity(intent)
         }
     }
 }
